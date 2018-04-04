e5c317c392ed21406ff0ae94eff9a775ca691a83
lmatz <martin823@126.com>
2018-04-04 17:18:05 +0000

    add constant.hpp for preprocess defined value

---
+++ core/utils/constant.hpp
@@ -0,0 +1,18 @@
+
+
+namespace myfs {
+
+#ifndef BLOCK_SIZE
+#define BLOCK_SIZE 512 
+#endif
+
+#ifndef NUM_BLOCKS
+#define NUM_BLOCKS 15
+#endif
+
+#ifndef THREADPOOL_SIZE
+#define THREADPOOL_SIZE 10
+#endif
+
+
+}
