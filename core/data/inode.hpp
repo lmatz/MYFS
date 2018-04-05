449f4aee839a1710fe6160dacbf387b986a0bed3
lmatz <martin823@126.com>
2018-04-05 19:00:41 +0000

    add macro for file mode in inode

--- core/data/inode.hpp
+++ core/data/inode.hpp
@@ -4,6 +4,22 @@
 
 namespace myfs {
 
+#define S_IFMT      0170000
+#define S_IFSOCK    0140000
+#define S_IFLNK     0120000
+#define S_IFREG     0100000
+#define S_IFBLK     0060000
+#define S_IFDIR     0040000
+#define S_IFCHR     0020000
+#define S_IFIFO     0010000
+
+#define S_ISREG(m)  (((m) & S_IFMT) == S_IFREG)
+#define S_ISDIR(m)	(((m) & S_IFMT) == S_IFDIR)
+#define S_ISCHR(m)	(((m) & S_IFMT) == S_IFCHR)
+#define S_ISBLK(m)	(((m) & S_IFMT) == S_IFBLK)
+#define S_ISFIFO(m) (((m) & S_IFMT) == S_IFIFO)
+#define S_ISLNK(m)	(((m) & S_IFMT) == S_ISLNK)
+#define S_ISSOCK(m) (((m) & S_IFMT) == S_IFSOCK)
 	struct myfs_inode {
 		uint16_t	i_mode; 	/* File mode */
 		uint16_t	i_uid;  	/* Owner Uid */
