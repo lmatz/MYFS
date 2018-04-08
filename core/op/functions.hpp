a43f6b785fe854202f0c4015e37117609e8107d1
lmatz <martin823@126.com>
2018-04-08 11:53:08 +0000

    add straccess function to show mask in access() nicely

--- core/op/functions.hpp
+++ core/op/functions.hpp
@@ -3,9 +3,13 @@
 
 namespace myfs {
 
+	// check whether the 'path' is valid or not (Not checking existence)
 	int check_path(const char *path);
 
 	// print mode_t nicely
 	void strmode(mode_t mode, char *buf);
+
+	// print masj in access() nicely
+	void straccess(int mask, char *buf);
 
 }
